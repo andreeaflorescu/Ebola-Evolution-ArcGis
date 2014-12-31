d43a546ebbcd4c8c7fcf5a77d959a73efd42a0f1
andreea.florescu <andreea.florescu@cti.pub.ro>
2014-12-31 02:11:04 +0200

    added mock-up popup on click on simplemarker

--- scripts/mapFunctions.js
+++ scripts/mapFunctions.js
@@ -2,6 +2,7 @@
  * Created by Cristina on 12/30/14.
  */
 var map;
+var red = [0,0,255,1];
 
 require([
     "esri/map",
@@ -10,7 +11,8 @@ require([
     "esri/geometry/Point",
     "esri/symbols/SimpleMarkerSymbol",
     "esri/graphic",
-    "dojo/_base/array"
+    "dojo/_base/array",
+    "esri/InfoTemplate"
 ], function(
     Map,
     FeatureLayer,
@@ -18,7 +20,8 @@ require([
     Point,
     SimpleMarkerSymbol,
     Graphic,
-    arrayUtils
+    arrayUtils,
+    InfoTemplate
     ) {
 
     map = new Map("map",{
@@ -29,10 +32,23 @@ require([
 
     map.on("load", function(){
         var points = [[19.82,41.33],[16.37,48.21],[18.38,43.85],[23.32,42.7],[16,45.8],[19.08,47.5],[12.48,41.9],[21.17,42.67],[21.43,42],[19.26,42.44],[26.1,44.43],[12.45,43.93],[20.47,44.82],[17.12,48.15],[14.51,46.06],[12.45,41.9]];
-        var initColor = "#ce641d";
+        var initColor = [0,0,255,1];
         arrayUtils.forEach(points, function(point) {
             var graphic = new Graphic(new Point(point), createSymbol(initColor));
+
+            var json = {
+                title:"Attributes",
+                content:"<tr>State Name: <td>State</tr></td><br><tr>Population: <td>200</tr></td>"
+            }
+            var infoTemplate = new InfoTemplate(json);
+            graphic.setInfoTemplate(infoTemplate);
+
             map.graphics.add(graphic);
+        });
+        map.graphics.on("click", function(e){
+            //get the associated node info when the graphic is clicked
+            var node = e.graphic.getNode();
+            console.log(node);
         });
     });
 
@@ -50,10 +66,10 @@ require([
         }
     });
 
-    function createSymbol(color){
+    function createSymbol(color, size){
         var markerSymbol = new esri.symbol.SimpleMarkerSymbol();
-        markerSymbol.setColor(new dojo.Color([0,0,255,1]));
-        markerSymbol.setSize(10);
+        markerSymbol.setColor(new dojo.Color(color));
+        markerSymbol.setSize(size);
         markerSymbol.setOutline(null);
         return markerSymbol;
     }
